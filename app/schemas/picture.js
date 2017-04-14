f4743a0842ee3800b34549fdc9453012b44f3b84
zhaoxuetong <zhaoxuetong@chinahomu.com>
2017-04-14 14:00:23 +0800

    picture

--- app/schemas/picture.js
+++ app/schemas/picture.js
@@ -1,2 +1,40 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+let PictureSchema = new Schema({
+    room: {
+        type: String
+    },
+    position: {
+        type: String
+    },
+    alt: String,
+    url: String,
+    urlBack: String,
+    rank: Number,
+    uploader: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    createAt: {
+        type: Date
+        , default: Date.now
+    },
+    updateAt: {
+        type: Date
+        , default: Date.now
+    }
+});
+
+PictureSchema.pre('save',function(next){
+   const picture = this;
+   if(picture.isNew){
+       picture.createAt = message.updateAt = Date.now;
+   }else{
+       picture.updateAt = Date.now;
+   }
+   next();
+});
